167b3c7d17e8e251036189caa6c8305c20721b6a
Roxny <shtanke07alex@gmail.com>
2026-03-13 10:07:21 +0300

    added sql-tables for catalog

---
+++ catalog_db.sql
@@ -0,0 +1,86 @@
+CREATE TABLE directors
+(
+	director_id serial PRIMARY KEY,
+	director_name varchar(256) UNIQUE NOT NULL
+);
+
+CREATE TABLE studios
+(
+	studio_id serial PRIMARY KEY,
+	studio_name varchar(256) UNIQUE NOT NULL
+);
+
+CREATE TABLE genres
+(
+	genre_id serial PRIMARY KEY,
+	genre_name varchar(256) UNIQUE NOT NULL
+);
+
+CREATE TABLE actors
+(
+	actor_id serial PRIMARY KEY,
+	actor_name varchar(256) UNIQUE NOT NULL
+);
+
+CREATE TABLE countries
+(
+	country_id serial PRIMARY KEY,
+	country_name varchar(256) UNIQUE NOT NULL
+);
+
+CREATE TABLE platforms
+(
+	platform_id serial PRIMARY KEY,
+	platform_name varchar(256) UNIQUE NOT NULL
+);
+
+CREATE TABLE projects
+(
+	project_id serial PRIMARY KEY,
+	title varchar(256) UNIQUE NOT NULL,
+	type varchar(256) CHECK (type IN('serial','movie')) NOT NULL,
+	date_of_release date NOT NULL,
+	rating decimal (3,1),
+	description text,
+	director_id int,
+	studio_id int,
+	country_id int,
+
+	FOREIGN KEY (director_id) REFERENCES directors(director_id),
+	FOREIGN KEY (studio_id) REFERENCES studios(studio_id),
+	FOREIGN KEY (country_id) REFERENCES countries(country_id)
+);
+
+CREATE TABLE project_genre
+(
+	project_id int,
+	genre_id int,
+	
+	PRIMARY KEY(project_id,genre_id),
+
+	FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+);
+
+CREATE TABLE project_cast
+(
+	project_id int,
+	actor_id int,
+	role varchar(256) NOT NULL,
+
+	PRIMARY KEY(project_id, actor_id),
+
+	FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
+    FOREIGN KEY (actor_id) REFERENCES actors(actor_id)
+);
+
+CREATE TABLE project_on_platform
+(
+	project_id int,
+	platform_id int,
+
+	PRIMARY KEY(project_id, platform_id),
+
+	FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE,
+    FOREIGN KEY (platform_id) REFERENCES platforms(platform_id)
+)
